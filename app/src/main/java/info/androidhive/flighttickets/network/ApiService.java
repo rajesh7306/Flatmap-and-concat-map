94dc3b3d6223fee6e45b9a0a637105df33ba9cbf
Rajesh <rajeshswayampu96@gmail.com>
2022-02-16 21:48:01 +0530

    commit

---
+++ app/src/main/java/info/androidhive/flighttickets/network/ApiService.java
@@ -0,0 +1,20 @@
+package info.androidhive.flighttickets.network;
+
+
+
+import java.util.List;
+
+import info.androidhive.flighttickets.network.model.Price;
+import info.androidhive.flighttickets.network.model.Ticket;
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiService {
+
+    @GET("airline-tickets.php")
+    Single<List<Ticket>> searchTickets(@Query("from") String from, @Query("to") String to);
+
+    @GET("airline-tickets-price.php")
+    Single<Price> getPrice(@Query("flight_number") String flightNumber, @Query("from") String from, @Query("to") String to);
+}
